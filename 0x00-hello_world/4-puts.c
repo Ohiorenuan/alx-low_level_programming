4d6ed50308ad54f095db0c854043a65050ebced9
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-07 16:33:52 -0700

    Just trying to please betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* Well here is a description betty */
 int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle" );
   return(0);
 }
