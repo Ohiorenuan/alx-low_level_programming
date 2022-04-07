5c5f1900266f84cd11723f034d96ff529c91cacd
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-07 16:45:16 -0700

    Still betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* Well here is a description betty */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle" );
-  return(0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main()
+/*Heres the description */
+int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
-        return(0);
+  printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
 }
