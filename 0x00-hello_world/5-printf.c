4c3d2d9154339a26d305057f78a62f6fc4021643
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-07 16:54:43 -0700

    Something not betty related

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
 /*Heres the description */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-        return (0);
+  printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+  return (0);
 }
