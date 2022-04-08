ca757de4a33296c0ca7d380ee417dc5bc0dbe34e
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-07 17:29:42 -0700

    learnt how to setup the way betty wants

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/* Well here is a description betty */
+/**
+ * main - Well here is a description betty
+ *
+ * Return: well it returns 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/*Heres the description */
+/**
+ * main - heres the description
+ *
+ *Return: well it gives a 0
+ */
 int main(void)
 {
-  printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-  return (0);
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
