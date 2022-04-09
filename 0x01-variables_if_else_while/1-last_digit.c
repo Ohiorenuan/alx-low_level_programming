fcebf87ed939c6759a3c9ccaca25a7d3e977e45b
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-09 05:23:15 +0100

    edited 1-last_digit.c and created 2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - the description for main
  *
@@ -12,8 +13,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d %s\n", n, n % 10, (((n % 10) > 5) ? "and is greater than 5"
-							  : "and is less than 6 and not 0")
-	       : ((n % 10) == 5) ? "and is 0");
+	printf("Last digit of %d is %d %s\n", n, n % 10,
+	       ((n % 10) == 0) ? "and is 0"
+	       : (((n % 10) > 5) ? "and is greater than 5"
+		  : "and is less than 6 and not 0"));
 	return (0);
 }
