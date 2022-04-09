8745500a22cec60e1082156300d02cec44da24a9
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-09 06:09:31 +0100

    Created 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - description
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int h;
+
+
+	for (h = '0'; h <= '9'; ++h)
+		putchar(h);
+	putchar('\n');
+
+	return (0);
+}
