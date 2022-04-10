d73002a6c7c8e1e95759a3fc4784e0ee7f8a8ae0
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-10 14:57:35 +0100

    edited 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - describtion on main
+ *
+ *Return: well more describtion
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+
+	return (1);
+}
