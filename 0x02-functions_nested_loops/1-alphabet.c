95611a0ef30b366551a9648a97cc874f1a138f2e
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-12 22:54:26 +0100

    Created the files 1-alphabet.c and 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - well describtion
+ */
+
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for
+		(alph = 'a';
+		 alph <= 'z';
+		 alph++);
+
+	_putchar(alph);
+	_putchar('\n');
+}
