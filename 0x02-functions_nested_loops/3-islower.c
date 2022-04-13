acdd2e4973c3781e0c67946d6fa82dc946a49131
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-13 19:18:03 +0100

    Edited main.h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - description
+ *int _islower(int c) - checks for lowercase character
+ *
+ *Return: 1 for lowercase and 0 otherwise
+ */
+
+int main(void)
+{
+	int _islower(int c)
+	{
+		
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
