104736d0a90a35bbf7305c9800f49467eda6dac3
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-17 22:06:08 +0100

    Made changes to 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Prints the alphabet
  */
 void print_alphabet(void)
