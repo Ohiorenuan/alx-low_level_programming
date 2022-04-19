4459d0adadc28c89d5ec27ba34cd156ca7a56a64
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-19 08:51:49 +0100

    more betty related issues on 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,13 +13,12 @@ int main(void)
 
 	i = 0;
 /**
- * Describtion - The while loop doesn't have a increament expression
+ * Description - The while loop doesn't have a increament expression
  *        while (i < 10)
  *        {
  *               putchar(i);
  *        }
  */
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
