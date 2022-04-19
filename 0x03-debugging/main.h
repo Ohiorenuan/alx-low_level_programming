01a2169c524e3b0fc4e5fdb1781c59a62351a92b
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-19 07:56:40 +0100

    Edited main.h and 0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,20 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - Tests function if its positive or negative
+ * @i: Integer to be tested
+ */
 void positive_or_negative(int i);
+
+/**
+ * largest_number - Tests for the largest number
+ * @a: One of the number being evaluated
+ * @b: One of the number being evaluated
+ * @c: One of the number being evaluated
+ *
+ * Return: The largest number
+ */
 int largest_number(int a, int b, int c);
 
 #endif
