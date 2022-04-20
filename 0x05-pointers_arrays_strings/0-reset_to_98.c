cbf3a42f062c48cd6ef916ba42279b16a6a02f5f
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-20 12:02:39 +0100

    Created the files 0-reset_to_98.c and 1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer to an int and updates the value
+ * @n: Pointer to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,4 +9,17 @@
  */
 int _putchar(char c);
 
+/**
+ * reset_to_98 - Takes a pointer to an int and updates the value
+ * @n: Pointer to be updated
+ */
+void reset_to_98(int *n);
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Pointer to be swapped
+ * @b: Pointer to be swapped
+ */
+void swap_int(int *a, int *b);
+
 #endif
