55f58ee35e0eeb41ea3616c8130e274488ccbbc6
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-20 11:37:24 +0100

    Created the folder 0x05-pointers_arrays_strings, a readme, gitignore, test.c and main.h file. copied _putchar.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - It prints out a single character
+ * @c: The character to be printed
+ *
+ * Return: The character c
+ */
+int _putchar(char c);
+
+#endif
