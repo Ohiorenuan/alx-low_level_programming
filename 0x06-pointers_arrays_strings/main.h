645470dbef3f049b71b35f25d815954f85b586ae
Ohiorenuan Akele <akeleohio@gmail.com>
2022-04-22 09:56:48 +0100

    Created the dir 0x06-pointers_arrays_strings, the readme file, the header main.h and copied in _putchar.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Prints out a character
+ * @c: The character to be printed
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+#endif
