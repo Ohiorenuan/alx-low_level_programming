c96b7e61fe8a601b995934b4308d3b2549f69ee1
Ohiorenuan Akele <akeleohio@gmail.com>
2022-05-02 19:06:16 +0100

    made 0-whatsmyname betty compliant

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main (int argc __attribute__((unused)), char *argv[])
+/**
+ * main - Entry point of function
+ * @argc: The count of arguments
+ * @argv: Array of arguements
+ * Return: The value 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
