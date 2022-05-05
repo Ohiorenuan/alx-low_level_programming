5ea0e04afd583c24313cbd79390a63b40a079187
Ohiorenuan Akele <akeleohio@gmail.com>
2022-05-05 15:08:10 +0100

    Debugging 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,8 @@ char *str_concat(char *s1, char *s2)
 		size1++;
 	for (i = 0; s2[i]; i++)
 		size2++;
-	dest = malloc((size1 + size2) * sizeof(char) + 1);
-	if (dest != 0)
-	{
+	if (size1 != 0 && size2 != 0)
+	{ dest = malloc((size1 + size2) * sizeof(char) + 1);
 		for (i = 0; s1[i]; i++)
 			dest[i] = s1[i];
 		for (i = 0; i <= size2; i++)
