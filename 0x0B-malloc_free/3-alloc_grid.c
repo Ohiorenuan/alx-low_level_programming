5c2232c45d8a90f75b30a33f54a8d4e413f778be
Ohiorenuan Akele <akeleohio@gmail.com>
2022-05-06 09:47:32 +0100

    Debugging 3-alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 && height == 0)
 		dest = NULL;
 	else
-	{ dest = (int **)malloc(height * sizeof(int));
+	{ dest = (int **)malloc(height * sizeof(int *));
 		for (i = 0; i < height; i++)
 			dest[i] = malloc(width * sizeof(int));
 		if (dest != NULL)
