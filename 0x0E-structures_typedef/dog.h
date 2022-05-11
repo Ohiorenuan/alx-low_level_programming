d6bbff7b3365df247c7d718cafe2b20560e61518
Ohiorenuan Akele <akeleohio@gmail.com>
2022-05-11 11:29:43 +0100

    Created dir, files in it, and modified dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef __DOG__
+#define __DOG__
+
+/**
+ * struct dog - Contains info about dogs
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
