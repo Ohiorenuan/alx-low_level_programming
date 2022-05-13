f2fc18a16db44934b8a71b2b79bae0edae71c78e
Ohiorenuan Akele <akeleohio@gmail.com>
2022-05-13 13:53:58 +0100

    Created readme, necessary C files and headers, Added necessary prototypes to headers

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,44 @@
+#ifndef __3_CALC__
+#define __3_CALC__
+
+/**
+ * op_add - Returns sum of two numbers
+ * @a: One of the number to be operated on
+ * @b: One of the number to be operated on
+ * Return: - Sum of a and b
+ */
+int op_add(int a, int b);
+
+/**
+ * op_div - Returns result of the division of two numbers
+ * @a: One of the number to be operated on
+ * @b: One of the number to be operated on
+ * Return: - Result of division of a and b
+ */
+int op_div(int a, int b);
+
+/**
+ * op_sub - Returns difference of two numbers
+ * @a: One of the number to be operated on
+ * @b: One of the number to be operated on
+ * Return: - Difference of a and b
+ */
+int op_sub(int a, int b);
+
+/**
+ * op_mul - Returns sum of two numbers
+ * @a: One of the number to be operated on
+ * @b: One of the number to be operated on
+ * Return: - Multiple of a and b
+ */
+int op_mul(int a, int b);
+
+/**
+ * op_mod - Returns remainder of division of two numbers
+ * @a: One of the number to be operated on
+ * @b: One of the number to be operated on
+ * Return: - Remainder of division of a and b
+ */
+int op_mod(int a, int b);
+
+#endif
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,28 @@
+#ifndef __FUNCTIONS_POINTERS__
+#define __FUNCTIONS_POINTERS__
+/**
+ * print_name - Prints a name
+ * @name: The name to be printed
+ * @f: A pointer to a function
+ */
+void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - Executes a function on each element of an array
+ * @array: The array to be iterated
+ * @size: The size of the array
+ * @action: Pointer to the funtion to be executed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+/**
+ * int_index - Searches for an integer
+ * @array: The array being searched
+ * @size: Number of elements in the array
+ * @cmp: Pointer to the funtion used to compare values
+ * Return: Index of the first element for which cmp does not return 0
+ * or returns 1 if no element matches or size < 1
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
